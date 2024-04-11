dd8f616192139cfea12600a94b775d06296a5f9d
RupamDas-ts <dasrupam740@gmail.com>
2024-04-12 00:48:54 +0530

    Small Runner Change to try running parallel and added logger dependency

--- src/main/java/TestRunner/Runner.java
+++ src/main/java/TestRunner/Runner.java
@@ -2,9 +2,15 @@ package TestRunner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @CucumberOptions(features = "src/main/java", glue = { "FeatureFiles", "TestRunner", "StepDefinations" }, plugin = {
   "pretty", "html:target/cucumber-reports" }, tags = "@flightBooking") @Test public class Runner
   extends AbstractTestNGCucumberTests {
+  @Override
+  @DataProvider(parallel = true)
+  public Object[][] scenarios() {
+    return super.scenarios();
+  }
 }
